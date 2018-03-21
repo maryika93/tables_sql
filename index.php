9250ea9452aa0cdd31d1998af72b5cfcd6965a44
Maria <maryika93@mail.ru>
2018-03-21 22:49:17 +0600

    td

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+$servername = "localhost";
+$username = "mtipikina";
+$password = "neto1539";
+$dbname = "mtipikina";
+session_start();
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    $sql
+        = "CREATE TABLE `Car` ( `ID` INT NOT NULL AUTO_INCREMENT, 
+                                  `name` VARCHAR(15) NOT NULL, 
+                                  `price` INT NOT NULL, 
+                                  `color` VARCHAR(15) NOT NULL,
+                                  PRIMARY KEY (`ID`)) ENGINE = InnoDB DEFAULT CHARSET = utf8";
+    $conn->exec($sql);
+if(isset($_GET['db'])) {
+    $getdb = $_GET['db'];
+    $_SESSION['getdb'] = $getdb;
+}
+if(isset($_GET['delete'])) {
+    $del = $_GET['delete'];
+    $db = $_SESSION['getdb'];
+    $tbl = $conn->query("ALTER TABLE $db DROP `$del`");
+}
+    if(isset($_GET['changetype'])) {
+        $cht = $_GET['changetype'];
+        $db = $_SESSION['getdb'];
+        //$type = $_POST['type'];
+        echo '<pre>';
+        print_r($_POST);
+        $tbl = $conn->query("ALTER TABLE $db CHANGE `$cht` `$cht` $type");
+    }
+}
+catch(PDOException $e)
+{
+    die("Error: " . $e->getMessage());
+}
+?>
+
+<h1> Список таблиц в базе данных <?php echo $dbname ?> </h1>
+    <?php
+$tbl = $conn->query('SHOW TABLES');
+foreach ($tbl as $table){
+    echo '<li><a href="index.php?db='. $table['Tables_in_mtipikina'].'">'.$table['Tables_in_mtipikina'].'</a></li><br>';
+    }
+    ?>
+<table border="1", cellpadding="10", width="100%">
+    <tr>
+        <td align="center"><b> Field </b></td>
+        <td align="center"><b> Type </b></td>
+        <td align="center"><b> Null </b></td>
+        <td align="center"><b> Key </b></td>
+        <td align="center"><b> Default </b></td>
+        <td align="center"><b> Extra </b></td>
+        <td align="center"><b> Редактирование </b></td>
+    </tr>
+
+    <?php
+    if(isset($_GET['db'])) {
+    $gdb = $_GET['db'];
+    $tbl = $conn->query("DESCRIBE $gdb");
+    foreach($tbl as $table) {
+        $d = $table['Field']; ?>
+        <tr>
+            <td align="center"><?php echo $table['Field'] ?></td>
+            <td align="center"><?php echo $table['Type'] ?></td>
+            <td align="center"><?php echo $table['Null'] ?></td>
+            <td align="center"><?php echo $table['Key'] ?></td>
+            <td align="center"><?php echo $table['Default'] ?></td>
+            <td align="center"><?php echo $table['Extra'] ?></td>
+            <td align="center">
+                <form method="post" action="" enctype="multipart/form-data">
+                    <?php echo '<a href="index.php?delete=' . $table['Field'] . '">Удалить поле</a> <br/>';
+                    echo "<select name = 'type'>";
+                    $data1=[];
+                    $data1 = array('INT', 'VARCHAR', 'TEXT', 'DATE');
+                    for($i=0; $i<count($data1); $i++) {
+                        $a = $data1[$i];
+                        echo "<option value = '$a' > $a </option>";
+                    }
+                    echo "</select>"; ?>
+                    <?php echo '<a href="index.php?changetype=' . $table['Type'] . '">Изменить тип поля</a> <br/>'?>
+                    <input type="text" name="name" placeholder="Имя">
+                    <?php echo '<a href="index.php?changename=' . $table['Field'] . '">Изменить имя поля</a> <br/>'?>
+                </form>
+            </td>
+        </tr>
+        <?php
+    }
+    }
+    ?>
+</table>
